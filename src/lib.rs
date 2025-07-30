1cd95b13d9bdb0eac03d93f43590a14c1287a241
Riddhima Srivastava <srivastavariddhima464@gmail.com>
2025-07-31 00:18:47 +0530

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+use wasm_bindgen::prelude::*;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+// Define shared state
+thread_local! {
+    static SECRET_NUMBER: Rc<RefCell<u32>> = Rc::new(RefCell::new(0));
+}
+
+#[wasm_bindgen]
+pub fn reset_game() {
+    use rand::Rng;
+    let secret = rand::thread_rng().gen_range(1..=100);
+    SECRET_NUMBER.with(|s| *s.borrow_mut() = secret);
+}
+
+#[wasm_bindgen]
+pub fn check_guess(guess: u32) -> String {
+    SECRET_NUMBER.with(|s| {
+        let secret = *s.borrow();
+        if guess < secret {
+            "Too small!".to_string()
+        } else if guess > secret {
+            "Too big!".to_string()
+        } else {
+            "You guessed it!".to_string()
+        }
+    })
+}
---
+++ static/main.js
@@ -0,0 +1,15 @@
+import init, { reset_game, check_guess } from "../pkg/guessing_game_web.js";
+
+async function run() {
+  await init();
+  reset_game();
+}
+
+window.makeGuess = () => {
+  const value = parseInt(document.getElementById("guess").value, 10);
+  const result = check_guess(value);
+  document.getElementById("result").textContent = result;
+};
+
+run();
+
